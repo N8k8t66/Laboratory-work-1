febc8505939031316076ef199df1401916bca062
N8k8t66 <nikita.nunizyamov@yandex.ru>
2026-06-01 16:19:48 +0400

    Update CalculatorModel.cs

--- TP/CalculatorModel.cs
+++ TP/CalculatorModel.cs
@@ -8,7 +8,10 @@ namespace CalculatorApp.Models
 {
     public class CalculatorModel
     {
+        [Required(ErrorMessage = "Поле 'Первое число' обязательно для заполнения")]
         public long Operand1 { get; set; }
+        [Required(ErrorMessage = "Поле 'Второе число' обязательно для заполнения")]
+        [Compare("Operand1", ErrorMessage = "Второе число должно совпадать с первым числом")]
         public decimal Operand2 { get; set; }
         public string Operation { get; set; }
         public decimal result { get; set; }
